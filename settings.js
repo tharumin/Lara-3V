87cc11ae7cc64f89acee88fb15c15b2828d40c74
Isaacnoriel <98013450+Isaacnoriel@users.noreply.github.com>
2025-12-04 18:58:13 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
-PREFIX: process.env.PREFIX || ".",
+SESSION_ID: process.env.SESSION_ID || "3JwUCb6Y#-6NjQ9zW4o9l9W9yCkrd1p6aZ_SYVC3LSRLweqZZGLc", // Add your session id
+PREFIX: process.env.PREFIX || "*",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
 DEV: process.env.DEV || "94779062397",
