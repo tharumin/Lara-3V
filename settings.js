989a626f2e13b14b339a0c3c30216d44dc3b6e7e
Gaming2224-podiya <accgit89@gmail.com>
2026-03-23 08:26:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
     OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94740707157' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
     SESSION_ID: process.env.SESSION_ID === undefined ? '𝕃𝔸ℝ𝔸-𝕄𝔻=aY5nEaSa#11B4Bdcx2KcvEAVDpjW2QSvNG_vsjf3vIHP5vJfzsQQ' : process.env.SESSION_ID, //ADD YOUR SSION ID
     GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Gaming2224-podiya' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
 
 /*
